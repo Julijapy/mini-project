99535b38eb05a7846f89a0334b243b9150b3bacf
yuliya valeeva <jul.valeewa@yandex.ru>
2025-01-10 23:03:12 +0500

    added functions and dependency packages

---
+++ src/masks.py
@@ -0,0 +1,11 @@
+def get_mask_card_number(card_number: str) -> str:
+    """ Функция маскировки номера банковской карты"""
+    number_slice_1 = card_number[0:4]
+    number_slice_2 = card_number[4:6]
+    number_slice_3 = card_number[12:]
+    return "".join(number_slice_1) + " " + "".join(number_slice_2) + "** **** " + "".join(number_slice_3)
+
+
+def get_mask_account(account_number: str) -> str:
+    """ Функция маскировки номера банковского счета"""
+    return "**" + account_number[-4:]
