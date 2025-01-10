a16538bc1aebf190407dc690d9b7b3d80191ca1c
yuliya valeeva <jul.valeewa@yandex.ru>
2025-01-11 04:22:09 +0500

    formatting files

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,8 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(user_card: str) -> str:
-    """ Функция, которая маскирует номер или счет карты"""
+    """Функция, которая маскирует номер или счет карты"""
     details = user_card.split()
     card_number = get_mask_card_number(details[-1])
     card_account = get_mask_account(details[-1])
@@ -18,7 +18,7 @@ def mask_account_card(user_card: str) -> str:
 
 
 def get_date(info: str) -> str:
-    """ Функция, которая приводит данные о дате в удобный формат """
+    """Функция, которая приводит данные о дате в удобный формат"""
     date_info_slice = info[:10].split("-")
     formatted_date = date_info_slice[-1:-4:-1]
     return ".".join(formatted_date)
