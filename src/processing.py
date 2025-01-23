055b923343e4377928e493c5663ca9a570755501
yuliya valeeva <jul.valeewa@yandex.ru>
2025-01-24 00:00:05 +0500

    added docstring

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,6 @@
 def filter_by_state(data: list[{}], state="EXECUTED") -> list[{}]:
+    """ Функция возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению """
     new_list = []
     for i in data:
         for key, value in i.items():
@@ -9,5 +11,6 @@ def filter_by_state(data: list[{}], state="EXECUTED") -> list[{}]:
 
 
 def sort_by_date(date: list[{}], reverse=True) -> list[{}]:
+    """ Функция возвращает новый список, отсортированный по дате"""
     new_date = sorted(date, key=lambda x: x["date"], reverse=reverse)
     return new_date
