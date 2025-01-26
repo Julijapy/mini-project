e6b668fa55bb114581fa0e5fff63fa5217c3c2d8
yuliya valeeva <jul.valeewa@yandex.ru>
2025-01-26 16:39:57 +0500

    add README.md

--- tests/main.py
+++ tests/main.py
@@ -1,10 +1,10 @@
-# from src.widget import get_date, mask_account_card
+from src.widget import get_date, mask_account_card
 
 from src.processing import filter_by_state, sort_by_date
 
-# print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Счет 73654108430135874305"))
 
-# print(get_date("2024-03-11T02:26:18.671407"))
+print(get_date("2024-03-11T02:26:18.671407"))
 
 print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
