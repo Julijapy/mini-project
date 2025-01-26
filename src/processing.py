95650c921de01378d436816077f68e4052e04410
yuliya valeeva <jul.valeewa@yandex.ru>
2025-01-26 17:14:13 +0500

    formatting the processing.py file

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,11 @@
-def filter_by_state(data: list[{}], state="EXECUTED") -> list[{}]:
-    """ Функция возвращает новый список словарей, содержащий только те словари,
-    у которых ключ state соответствует указанному значению """
+from typing import Any
+
+
+def filter_by_state(
+    data: list[dict[str, Any]], state="EXECUTED"
+) -> list[dict[str, Any]]:
+    """Функция возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению"""
     new_list = []
     for i in data:
         for key, value in i.items():
@@ -10,7 +15,7 @@ def filter_by_state(data: list[{}], state="EXECUTED") -> list[{}]:
     return new_list
 
 
-def sort_by_date(date: list[{}], reverse=True) -> list[{}]:
-    """ Функция возвращает новый список, отсортированный по дате"""
+def sort_by_date(date: list[dict[str, Any]], reverse=True) -> list[dict[str, Any]]:
+    """Функция возвращает новый список, отсортированный по дате"""
     new_date = sorted(date, key=lambda x: x["date"], reverse=reverse)
     return new_date
