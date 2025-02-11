4066b5a814baf8243547a06a5f78883f95b7721c
yuliya valeeva <jul.valeewa@yandex.ru>
2025-02-11 23:14:39 +0500

    modified masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,12 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера банковской карты"""
-    number_slice_1 = card_number[0:4]
-    number_slice_2 = card_number[4:6]
-    number_slice_3 = card_number[12:]
-    return (
-        "".join(number_slice_1)
-        + " "
-        + "".join(number_slice_2)
-        + "** **** "
-        + "".join(number_slice_3)
-    )
+    if card_number.isdigit():
+        number_slice_1 = card_number[0:4]
+        number_slice_2 = card_number[4:6]
+        number_slice_3 = card_number[12:]
+    else:
+        raise ValueError("Данные введены некорректно")
+    return "".join(number_slice_1) + " " + "".join(number_slice_2) + "** **** " + "".join(number_slice_3)
 
 
 def get_mask_account(account_number: str) -> str:
