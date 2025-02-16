5036154fba4e16c8326315a3d3b807686d3baff5
yuliya valeeva <jul.valeewa@yandex.ru>
2025-02-17 01:03:38 +0500

    add test_mask_account_card func

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,5 +1,9 @@
+import pytest
+
 from src.widget import mask_account_card, get_date
 
-print(mask_account_card("Счет 73654108430135874305"))
-
-print(get_date("2024-03-11T02:26:18.671407"))
+@pytest.mark.parametrize("user_card, expected", [("Visa Platinum 1111222233334444", "Visa Platinum 1111 22** **** 4444"),
+                                                 ("Maestro 7000792289606361","Maestro 7000 79** **** 6361"),
+                                                 ("11112222333344445555", "**5555")])
+def test_mask_account_card(user_card, expected):
+    assert mask_account_card(user_card) == expected
