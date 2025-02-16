5182a43e67b579f0b8caf6dc21411af2108fabda
yuliya valeeva <jul.valeewa@yandex.ru>
2025-02-17 01:01:17 +0500

    modified mask_account_card func

--- src/widget.py
+++ src/widget.py
@@ -3,17 +3,17 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(user_card: str) -> str:
     """Функция, которая маскирует номер или счет карты"""
-    details = user_card.split()
-    card_number = get_mask_card_number(details[-1])
-    card_account = get_mask_account(details[-1])
+    card_details = user_card.split()
     details_list = []
-    for item in details:
+    for item in card_details:
         if item.isalpha():
             details_list.append(item)
-    if len(details[-1]) == 16:
-        details_list.append(card_number)
-    else:
-        details_list.append(card_account)
+        else:
+            if len(item) == 16:
+                card_number = get_mask_card_number(card_details[-1])
+            else:
+                card_number = get_mask_account(card_details[-1])
+            details_list.append(card_number)
     return " ".join(details_list)
 
 
