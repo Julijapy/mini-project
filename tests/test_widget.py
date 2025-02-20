30654ca5fb735500958496915b8ddebe6a5f4b64
yuliya valeeva <jul.valeewa@yandex.ru>
2025-02-20 22:01:51 +0500

    add test_mask_account_card_incorrect func

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,9 +1,27 @@
 import pytest
 
-from src.widget import mask_account_card, get_date
+from src.widget import mask_account_card
 
 @pytest.mark.parametrize("user_card, expected", [("Visa Platinum 1111222233334444", "Visa Platinum 1111 22** **** 4444"),
                                                  ("Maestro 7000792289606361","Maestro 7000 79** **** 6361"),
-                                                 ("11112222333344445555", "**5555")])
+                                                 ("Счет 11112222333344445555", "Счет **5555"),
+                                                 ("Счет 10002000300040005000", "Счет **5000")])
 def test_mask_account_card(user_card, expected):
     assert mask_account_card(user_card) == expected
+
+
+@pytest.mark.parametrize("user_card, expected", [("Visa Platinum 111122223333444", "Данные введены некорректно"),
+                                                 ("Visa 11112222333344445", "Данные введены некорректно"),
+                                                 ("Maestro", "Данные введены некорректно"),
+                                                 ("abrakadabra simsalabim", "Данные введены некорректно"),
+                                                 ("", "Данные введены некорректно"),
+                                                 (" ", "Данные введены некорректно"),
+                                                 ("Счет 1111222233334444", "Данные введены некорректно"),
+                                                 ("Счет 11112222333344445", "Данные введены некорректно"),
+                                                 ("Счет 100020003000400050006000", "Данные введены некорректно"),
+                                                 ("Счет abab", "Данные введены некорректно"),
+                                                 ("Счет a1a1", "Данные введены некорректно")])
+def test_mask_account_card_incorrect(user_card, expected):
+    with pytest.raises(ValueError) as exc_info:
+        mask_account_card(user_card)
+    assert str(exc_info.value) == expected
