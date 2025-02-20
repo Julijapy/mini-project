4b6670ff1384ca50fe17a37cf2c46a97c7a61666
yuliya valeeva <jul.valeewa@yandex.ru>
2025-02-20 22:00:52 +0500

    modified widget.py

--- src/widget.py
+++ src/widget.py
@@ -4,17 +4,25 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(user_card: str) -> str:
     """Функция, которая маскирует номер или счет карты"""
     card_details = user_card.split()
-    details_list = []
+    card_type = []
     for item in card_details:
         if item.isalpha():
-            details_list.append(item)
-        else:
-            if len(item) == 16:
-                card_number = get_mask_card_number(card_details[-1])
+            card_type.append(item)
+        elif item.isdigit():
+            if  "Счет" in card_details and len(item) == 20:
+                card_number = get_mask_account(item)
+            elif  "Счет" in card_details and len(item) == 16:
+                raise ValueError("Данные введены некорректно")
+            elif len(item) == 16:
+                card_number = get_mask_card_number(item)
             else:
-                card_number = get_mask_account(card_details[-1])
-            details_list.append(card_number)
-    return " ".join(details_list)
+                raise ValueError("Данные введены некорректно")
+            card_type.append(card_number)
+    if not card_details:
+        raise ValueError("Данные введены некорректно")
+    elif not card_details[-1].isdigit():
+        raise ValueError("Данные введены некорректно")
+    return " ".join(card_type)
 
 
 def get_date(info: str) -> str:
