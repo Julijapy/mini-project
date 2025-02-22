583430a7b749b9e507c89c37526dfd78bde525cc
yuliya valeeva <jul.valeewa@yandex.ru>
2025-02-22 11:55:52 +0500

    add test_get_date_normal, test_get_date_incorrect func

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_date
 
 @pytest.mark.parametrize("user_card, expected", [("Visa Platinum 1111222233334444", "Visa Platinum 1111 22** **** 4444"),
                                                  ("Maestro 7000792289606361","Maestro 7000 79** **** 6361"),
@@ -24,4 +24,24 @@ def test_mask_account_card(user_card, expected):
 def test_mask_account_card_incorrect(user_card, expected):
     with pytest.raises(ValueError) as exc_info:
         mask_account_card(user_card)
+    assert str(exc_info.value) == expected
+
+
+@pytest.mark.parametrize("date_info, expected", [("2025.02.21", "21.02.2025"),
+                                                 ("2025/02/21", "21.02.2025"),
+                                                 ("2025-02-21", "21.02.2025"),
+                                                 ("2024-03-11T02:26:18.671407", "11.03.2024"),
+                                                 ("2024/03/11T02:26:18.671407", "11.03.2024"),
+                                                 ("2024.03.11T02/26/18/671407", "11.03.2024")])
+def test_get_date_normal(date_info, expected):
+    assert get_date(date_info) == expected
+
+
+@pytest.mark.parametrize("date_info, expected", [("", "Некорректная дата"),
+                                                 (" ", "Некорректная дата"),
+                                                 ("11111111", "Некорректная дата"),
+                                                 ("qwerty.123", "Некорректная дата")])
+def test_get_date_incorrect(date_info, expected):
+    with pytest.raises(ValueError) as exc_info:
+        get_date(date_info)
     assert str(exc_info.value) == expected
