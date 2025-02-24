d47cc4b5e3e22310e3dd3a646352a3b4c72f63df
yuliya valeeva <jul.valeewa@yandex.ru>
2025-02-24 15:59:47 +0500

    add fixture for filter_by_state func

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,38 @@ import pytest
 
 
 @pytest.fixture
-def card_number():
-    return ["1000222233skypro", "", "3000    60607070"]
+def data_normal():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+@pytest.fixture
+def filtering_result_normal():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+@pytest.fixture
+def data_state_empty():
+    return [{'id': 41428829, 'state': '', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': '', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+@pytest.fixture
+def filtering_result_state_empty():
+    return [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+@pytest.fixture
+def data_lack_of_status_executed():
+    return [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+@pytest.fixture
+def filtering_result_lack_of_status_executed():
+    return "Список пуст"
