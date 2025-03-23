d77aa7e606dabca76cee327170534a5bbb93c3aa
yuliya valeeva <jul.valeewa@yandex.ru>
2025-03-24 01:07:11 +0500

    add transaction_descriptions func

--- src/generators.py
+++ src/generators.py
@@ -5,3 +5,13 @@ def filter_by_currency(all_transactions: list[dict[str, Any]], name_of_the_curre
     usd_transactions = filter(lambda transaction: transaction["operationAmount"]["currency"]["code"] == name_of_the_currency, all_transactions)
     for item in usd_transactions:
         yield item
+
+
+def transaction_descriptions(all_transactions: list[dict[str, Any]]) -> Iterator[dict[[str, Any]]]:
+    description = map(lambda transaction: transaction["description"], all_transactions)
+    for item in description:
+        yield item
+
+
+def card_number_generator(start, stop):
+    pass
