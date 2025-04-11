cb2efc65e8dc226ee835aa2f2043553e181877a9
yuliya valeeva <jul.valeewa@yandex.ru>
2025-04-12 00:15:11 +0500

    fixes log func

--- src/decorators.py
+++ src/decorators.py
@@ -1,21 +1,26 @@
-def log(filename=None): # filename = необяз параметр.
-    def decorator(func):
+from functools import wraps
+
+
+def log(filename=None):
+    def my_decor(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
                 log_info = f'{func.__name__} ok'
                 if filename:
-                    with open(filename, 'a') as text:
+                    with open(filename, 'a', encoding="utf-8") as text:
                         text.write(log_info)
                 else:
                     print(log_info)
-                return result
             except Exception as e:
                 log_error = f'{func.__name__} error: {type(e).__name__}.Inputs: {args}, {kwargs}'
                 if filename:
-                    with open(filename, 'a') as text:
+                    with open(filename, 'a', encoding="utf-8") as text:
                         text.write(log_error)
                 else:
                     print(log_error)
+                raise e
+            return result
         return wrapper
-    return decorator
+    return my_decor
