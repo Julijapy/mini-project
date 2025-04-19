8bcf1563a130ee92755e61eecfc3a76ca8caa846
yuliya valeeva <jul.valeewa@yandex.ru>
2025-04-20 02:26:37 +0500

    fixing the log decorator

--- src/decorators.py
+++ src/decorators.py
@@ -2,28 +2,31 @@ from functools import wraps
 
 
 def log(filename=None):
-    """ Декоратор, который автоматически регистрирует детали выполнения функций"""
+    """Декоратор, который автоматически регистрирует детали выполнения функций"""
+
     def my_decor(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
-                log_info = f'{func.__name__} ok'
+                log_info = f"{func.__name__} ok"
                 if filename:
-                    with open(filename, 'a', encoding="utf-8") as text:
+                    with open(filename, "a", encoding="utf-8") as text:
                         text.write(log_info)
                         text.write("\n")
                 else:
                     print(log_info)
                 return result
             except Exception as e:
-                log_error = f'{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}'
+                log_error = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
                 if filename:
-                    with open(filename, 'a', encoding="utf-8") as text:
+                    with open(filename, "a", encoding="utf-8") as text:
                         text.write(log_error)
                         text.write("\n")
                 else:
                     print(log_error)
                 raise e
+
         return wrapper
+
     return my_decor
